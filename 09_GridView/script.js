e0bf1db527c27713c9eac27c99249b461d522c1c
Sergey Shaporov <sshaporov86@gmail.com>
2021-10-27 09:58:30 +0300

    gridView

---
+++ 09_GridView/script.js
@@ -0,0 +1,28 @@
+const dataExample = [
+    {
+      company: "Alfreds <b>Futterkiste</b>",
+      chef: "Maria Anders",
+      country: "Germany",
+    },
+    {
+      company: "Centro comercial Moctezuma",
+      chef: "Francisco Chang",
+      country: "Mexico",
+    },
+    {
+      company: "Ernst Handel",
+      chef: "Roland Mendel",
+      country: "Austria",
+    },
+    {
+      company: "Island Trading",
+      chef: "Helen Bennett",
+      country: "UK",
+    },
+    {
+      company: "Laughing Bacchus Winecellars",
+      chef: "Voshi Tannamuri",
+      country: "Canada",
+    },
+];
+
