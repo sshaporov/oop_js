a1bebd76bebf76af179f4c634e679c3f45b849f6
Sergey Shaporov <sshaporov86@gmail.com>
2021-10-27 17:03:30 +0300

    add localStorage class

---
+++ 10_Практика_по_нативному_JS/utils/localStorageUtil.js
@@ -0,0 +1,33 @@
+class LocalStorageUtil {
+    constructor() {
+        this.keyName = 'products'
+    }
+    getProducts() {
+        const products = localStorage.getItem(this.keyName)
+        if (products !== null) {
+            return JSON.parse(products)
+        }
+
+        return []
+    }
+    putProducts(id) {
+        const products = this.getProducts()
+        const index = products.indexOf(id)
+        let pushProduct = false
+
+        if (index === -1) {
+            products.push(id)
+            pushProduct = true
+        } else {
+            products.splice(index, 1)
+        }
+        localStorage.setItem(this.keyName, JSON.stringify(products))
+
+        return { pushProduct, products }
+    }
+}
+
+const a = new LocalStorageUtil()
+console.log(a.getProducts())
+a.putProducts("el10")
+a.putProducts("el11")
