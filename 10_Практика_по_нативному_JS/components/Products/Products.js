5f1f2716228d2c913bca6a152b10e77f66ecf940
Sergey Shaporov <sshaporov86@gmail.com>
2021-10-27 11:14:48 +0300

    add pruducts HTML

---
+++ 10_Практика_по_нативному_JS/components/Products/Products.js
@@ -0,0 +1,26 @@
+class Products {
+    render() {
+        let htmlCatalog = ''
+        CATALOG.forEach(({id, name, price, img}) => {
+            htmlCatalog += `
+                <li>
+                    <span>${name}</span>
+                    <img src="${img}" />
+                    <span>${price}</span>
+                    <button>Добавить в корзину</button>
+                </li>
+            `
+        })
+
+        let html = `
+            <ul>
+                ${htmlCatalog}
+            </ul>
+        `
+
+        ROOT_PRODUCTS.innerHTML = html
+    }
+}
+
+const productsPage = new Products()
+productsPage.render()
