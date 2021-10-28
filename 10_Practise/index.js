2048c0666e5445e86dfb88bed573de9810239160
Sergey Shaporov <sshaporov86@gmail.com>
2021-10-28 12:21:20 +0300

    add index.js

---
+++ 10_Practise/index.js
@@ -0,0 +1,17 @@
+function render() {
+    const productsStore = localStorageUtil.getProducts();
+    headerPage.render(productsStore.length);
+
+    productsPage.render()
+}
+
+let CATALOG = []
+
+fetch('server/catalog.json')
+    .then(res => res.json())
+    .then(body => {
+        CATALOG = body;
+        render();
+    })
+    .catch(err => console.log(err))
+
